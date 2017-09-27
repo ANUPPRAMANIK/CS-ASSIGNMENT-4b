f0c4d1a36c2bd84cc6b4d30936745617a27feb16
ANUP PRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-09-27 11:11:27 +0530

    Create Lab4b_question11.cpp

---
+++ Lab4b_question11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int gcd(int a, int b);
+main() {
+    int a,b,c;
+    cout<<" enter any two numbers ";
+    cin>>a>>b;
+    c=gcd(a,b);
+    cout<<" \n "<<" gcd of "<<a<<" and "<<b<<" is: "<<c;
+    }
+int gcd(int a,int b)
+{
+    if((a>=b)&&((a%b)==0))
+        return(b);
+    else
+        gcd(b,(a%b));
+}
